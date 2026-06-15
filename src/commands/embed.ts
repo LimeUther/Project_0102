16f023f2c2ef2e857f4325e1db50514dfe2e4bff
LimeUther <uther4ball@gmail.com>
2026-06-16 00:28:29 +0800

    Slash Commands

--- src/commands/embed.ts
+++ src/commands/embed.ts
@@ -1,15 +1,41 @@
-import { CommandInteraction, EmbedBuilder } from 'discord.js'
+import {
+  CommandInteraction,
+  EmbedBuilder,
+  MessageFlags,
+  SendableChannels,
+  SlashCommandBuilder
+} from 'discord.js'
 
 export default {
+
   name: 'embed',
   description: 'Generates an Embed within the channel',
+
   execute: async (interaction: CommandInteraction) => {
+    const channel = interaction.channel as SendableChannels;
+
     const embed = new EmbedBuilder()
-      .setColor("Aqua")
-      .setTitle("Test title")
-      .setDescription('Test Descriptino here lmao uhhhhhhh')
+    .setColor("Aqua")
+    .setTitle("Test title")
+    .setDescription('Test Descriptino here lmao uhhhhhhh')
 
+    await channel.send({ embeds: [embed] });
 
-    await interaction.reply({ embeds: [embed] });
-  }
+    await interaction.reply({
+      content: "message was sent",
+      flags: MessageFlags.Ephemeral
+    });
+  },
+
+  subcommands: [
+    {
+      name: 'test',
+      description: 'what',
+      options: [],
+      execute: () => {
+      },
+    },
+  ]
 }
+
+
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -8,7 +8,7 @@ import { dirname, join } from 'path'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const commands: { name: string, description: string}[] = [];
+const commands: { name: string, description: string, options: any[] }[] = [];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
@@ -16,8 +16,24 @@ try {
   console.log('Started refreshing application (/) commands.');
 
   await bot.handleFiles(join(__dirname, './commands/'), (file) => {
-    commands.push({ name: file.name, description: file.description });
+
+    file.options ??= []
+    file.subcommands ??= []
+
+    if (file.subcommands.length > 0) {
+      for (const { name, description, options } of file.subcommands) {
+        file.options.push({ name, description, options, type: 1 })
+      }
+    }
+
+    commands.push({
+      name: file.name,
+      description: file.description,
+      options: file.options
+    });
   })
+
+  console.log(commands)
 
   await rest.put(
     Routes.applicationCommands(process.env.CLIENT_ID),
